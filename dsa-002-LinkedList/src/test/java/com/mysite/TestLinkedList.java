4a0ad3a703d9934c77bd985b7cffe073de82084d
zhl <zhl3600274714@foxmail.com>
2023-12-13 22:50:22 +0800

    测试自定义的LinkedList<E>

---
+++ dsa-002-LinkedList/src/test/java/com/mysite/TestLinkedList.java
@@ -0,0 +1,13 @@
+package com.mysite;
+
+/**
+ * ClassName: TestLinkedList
+ * Package: com.mysite
+ * Description
+ *
+ * @Author zhl
+ * @Create 2023/12/13 20:02
+ * version 1.0
+ */
+public class TestLinkedList {
+}
