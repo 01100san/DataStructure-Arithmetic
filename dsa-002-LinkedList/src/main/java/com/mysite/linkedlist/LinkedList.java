30583f9edae7c3853255b34baf8a2eb54e5039dc
zhl <zhl3600274714@foxmail.com>
2023-12-14 22:43:35 +0800

    自定义LinkedList<E>单向链表

--- dsa-002-LinkedList/src/main/java/com/mysite/linkedlist/LinkedList.java
+++ dsa-002-LinkedList/src/main/java/com/mysite/linkedlist/LinkedList.java
@@ -32,55 +32,66 @@ public class LinkedList<E> extends AbstractList<E> implements Serializable, List
      */
     @Override
     public void add(int index, E element) {
-        rangeCheck(index);
+        /**
+         * 最好：O(1)
+         * 最坏：O(n)
+         * 平均：O(n)
+         */
+        rangeCheckForAdd(index);
 
-        //取出第一个节点
-        Node<E> node = first;
         //如果是第一个位置
         if (index == 0){
-            node = new Node<>(element,first);
+            first = new Node<>(element,first);
+            size ++;
+        }else {
+            //获取index前一个节点
+            Node<E> prev = node(index - 1);
+            //将prev的下一个节点next指向之前prev.next处的节点
+            prev.next  = new Node<>(element,prev.next);
+            size ++;
         }
 
-
-        //如果不是第一个节点前的位置
-        for (int i = 1; i <= index; i++) {
-            node = node.next;
-        }
-        //获取前一个节点
-        Node<E> tempNode = node(index);
-        Node<E> newNode = new Node<>(element,node);
-
-        tempNode.next = newNode;
-
-    }
+     }
 
     @Override
     public int indexOf(E element) {
-        //如果元素值和第一个节点的元素相等，直接返回第一个节点的索引
-        if (element.equals(first.element)) {
-            return 0;
-        }
-        //取出第一个节点的下一个节点
-        Node<E> node = first.next;
-        for (int index = 1; index < size; index++) {
-            if (element.equals(node.element)){
-                return index;
+        Node<E> node = first;
+        if (element == null){
+            for (int index = 0; index < size; index++) {
+                if (node.element == null){
+                    return index;
+                }
+                node = node.next;
             }
-            node = node.next;
+        }else {
+            for (int index = 0; index < size; index++) {
+                if (element.equals(node.element)){
+                    return index;
+                }
+                node = node.next;
+            }
         }
-
         return -1;
     }
 
     @Override
     public E get(int index) {
-        //得到index前的一个节点
+        /**
+         * 最好：O(1)
+         * 最坏：O(n)
+         * 平均：O(n)
+         */
         Node<E> node = node(index);
         return node.element;
     }
 
     @Override
     public E set(int index, E element) {
+        /**
+         * 最好：O(1)
+         * 最坏：O(n)
+         * 平均：O(n)
+         */
         //取出原来的index对应的node值
         Node<E> node = node(index);
         E oldEle = node.element;
@@ -90,13 +101,43 @@ public class LinkedList<E> extends AbstractList<E> implements Serializable, List
 
     @Override
     public E remove(int index) {
-        //待删除的节点
-        /*Node<E> node = node(index);
-        E element = node.element;
-        Node<E> next = node.next;
+        /**
+         * 最好：O(1)
+         * 最坏：O(n)
+         * 平均：O(n)
+         */
+        Node<E> oldNode = node(index);
+        E oldEle = oldNode.element;
 
-        return element;*/
-        return null;
+        //如果删除第一个节点
+        if (index == 0){
+            first = oldNode.next;
+            size --;
+        }else {
+            //取出删除前上一个节点
+            Node<E> prev = node(index - 1);
+            //将上一个节点的next指向要删除节点的下一个节点
+            prev.next = oldNode.next;
+            size --;
+        }
+
+        return oldEle;
+    }
+
+    @Override
+    public boolean remove(E element) {
+        /**
+         * 最好：O(1)
+         * 最坏：O(n)
+         * 平均：O(n)
+         */
+        int destIndex = indexOf(element);
+
+        if (destIndex == -1)
+            return false;
+
+        remove(destIndex);
+        return true;
     }
 
     /**
@@ -105,7 +146,13 @@ public class LinkedList<E> extends AbstractList<E> implements Serializable, List
      * @return 返回当前索引的节点
      */
     public Node<E> node(int index){
+        /**
+         * 最好：O(1)
+         * 最坏：O(n)
+         * 平均：O(n)
+         */
         rangeCheck(index);
+
         //遍历每一个节点
         Node<E> node = first;
         for (int i = 0; i < index; i++) {
@@ -122,5 +169,20 @@ public class LinkedList<E> extends AbstractList<E> implements Serializable, List
             this.element = element;
             this.next = next;
         }
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[");
+        for (int i = 0; i < size; i++) {
+            if (i != 0){
+                sb.append(", ");
+            }
+            sb.append(node(i).element);
+        }
+        sb.append("]");
+
+        return sb.toString();
     }
 }
