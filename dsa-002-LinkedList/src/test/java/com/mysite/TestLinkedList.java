711d474e29e8a0913262f9857bea81f10eda67db
zhl <zhl3600274714@foxmail.com>
2023-12-15 22:43:00 +0800

    测试LinkedList<e>双向链表

--- dsa-002-LinkedList/src/test/java/com/mysite/TestLinkedList.java
+++ dsa-002-LinkedList/src/test/java/com/mysite/TestLinkedList.java
@@ -1,6 +1,5 @@
 package com.mysite;
 
-import com.mysite.linkedlist.ArrayList;
 import com.mysite.linkedlist.LinkedList;
 import com.mysite.linkedlist.List;
 import org.junit.jupiter.api.Test;
@@ -11,37 +10,38 @@ import org.junit.jupiter.api.Test;
  * Description
  *
  * @Author zhl
- * @Create 2023/12/13 20:02
+ * @Create 2023/12/15 22:19
  * version 1.0
  */
 public class TestLinkedList {
     @Test
     public void test1(){
         List<Integer> list = new LinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-
-        list.add(null);
+        list.add(11);
         list.add(12);
+        list.add(13);
+        list.add(null);
+        list.add(14);
+        list.add(15);
 
-        list.add(list.size(),99);
+        //测试add(int index,E element)
+        //list.add(3,99);
+        //测试add插入头节点
+        //list.add(0,99);
+        //测试add插入尾节点
+        //list.add(list.size(),99);
 
         //测试remove(E element)
         /*boolean remove = list.remove(null);
         System.out.println(remove);*/
 
-        //测试add(int index,E element)
-        /*list.add(5,20);*/
+        //测试remove(int index)
+        /*Integer oldEle = list.remove(0);
+        System.out.println(oldEle);*/
 
         /*int count = list.indexOf(null);
         System.out.println(count);*/
 
-        //测试remove(int index)
-        /*Integer oldEle = list.remove(0);
-        System.out.println(oldEle);*/
 
         //测试indexOf(int index)
         /*int index = list.indexOf(2);
@@ -54,31 +54,5 @@ public class TestLinkedList {
         //测试size()
         System.out.println("节点的数量：" + list.size());
         System.out.println(list);
-    }
-
-    @Test
-    public void test2(){
-        java.util.List<Integer> list = new java.util.LinkedList<Integer>();
-        list.add(21);
-        int count = list.indexOf(1);
-        System.out.println(count);
-    }
-
-    /**
-     * 测试缩容和扩容
-     */
-    @Test
-    public void test3(){
-        List<Integer> list = new ArrayList<>();
-        /*java.util.List<Integer> list = new ArrayList<>();*/
-        for (int i = 0; i < 50; i++) {
-            list.add(i);
-        }
-        for (int i = 0; i < 50; i++) {
-            list.remove(0);
-        }
-
-        System.out.println(list);
-
     }
 }
