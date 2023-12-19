fae47c8661770ce766409b750ddc5696f33d73da
zhl <zhl3600274714@foxmail.com>
2023-12-19 22:39:45 +0800

    定义队列Queue<E>

--- dsa-004-Queue/src/main/java/com/mysite/queue/Queue.java
+++ dsa-004-Queue/src/main/java/com/mysite/queue/Queue.java
@@ -1,5 +1,8 @@
 package com.mysite.queue;
 
+import com.mysite.list.LinkedList;
+import com.mysite.list.List;
+
 /**
  * ClassName: Queue
  * Package: com.mysite.queue
@@ -9,6 +12,41 @@ package com.mysite.queue;
  * @Create 2023/12/18 22:25
  * version 1.0
  */
-public class Queue {
+public class Queue<E> {
+    private List<E> list = new LinkedList<>();
+    public int size(){
+        return list.size();
+    }
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
 
+    /**
+     * 入队
+     */
+    public void enQueue(E element){
+        list.add(element);
+    }
+
+    /**
+     * 出队
+     * @return
+     */
+    public E deQueue(){
+        return list.remove(0);
+    }
+
+    /**
+     * 获取队列的头元素
+     * @return
+     */
+    public E front(){
+        return list.get(0);
+    }
+    public void clear(){
+        list.clear();
+    }
 }
+
+
+
